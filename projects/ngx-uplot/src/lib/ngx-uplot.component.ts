329e5e1af3e2122067fe0e383cc45ad6a4c505cd
Alexey Oplachko <xirenec1@gmail.com>
2022-08-31 12:14:49 +0300

    Initial commit

---
+++ projects/ngx-uplot/src/lib/ngx-uplot.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-uplot',
+  template: `
+    <p>
+      ngx-uplot works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxUplotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
